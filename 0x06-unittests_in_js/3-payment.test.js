b67775c1c7969a81ba569139e11a293fea432982
Migue Tj <miguejustus@gmail.com>
2024-04-30 10:35:08 +0300

    Task 3: Add sendPaymentRequestToApi function and its tests using Sinon spy

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,13 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
+
+descibe('sendPaymentRequestToApi', function () {
+	it('should call calculateNumber wih SUM, 100, and 20', function () {
+		const spy = sinon.spy(Utils, 'calculateNumber');
+		sendPaymentRequestToApi(100, 20);
+		expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+		spy.restore();
+	});
+});
