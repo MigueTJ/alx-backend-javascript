c2338a33294a77a598421e1c8a77cf628f9a8e5e
Migue Tj <miguejustus@gmail.com>
2024-04-30 10:35:13 +0300

    Task 5: Add sendPaymentRequestToApi function and its tests using hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,25 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', function () {
+	let consoleLogSpy;
+
+	beforeEach(function () {
+		consoleLogSpy = sinon.spy(console, 'log');
+	});
+
+	afterEach(function () {
+		consoleLogSpy.restore();
+	});
+
+	it('should log "The total is: 120" when inputs are 100 and 20', function  () {
+		sendPaymentRequestToApi(100, 20);
+		expect(consoleLogSpy.calledonceWith('The total is: 120')).to.be.true;
+	});
+
+	it('should log "The total is: 20" when inputs are 10 and 10', function () {
+		sendPaymentRequestToApi(10, 10);
+		expect(consoleLogSpy.calledOnceWith('The total is: 20')).to.be.true;
+	});
+});
